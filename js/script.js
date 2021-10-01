550448345e428e05fc6e04b931170f0cf37e0de7
VladimirObidchuk <kremench@gmail.com>
2021-10-01 09:13:13 +0300

    final commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector(".burger");
+
+burger.addEventListener("click", () => {
+    document.body.classList.toggle("menu-open");
+});
+const burger1 = document.querySelector(".mobile__title");
+
+burger1.addEventListener("click", () => {
+    document.body.classList.toggle("menu-open");
+});
